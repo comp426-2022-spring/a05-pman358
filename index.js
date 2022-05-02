3781e10ed6b36ff6fee8a1e11e9d2a42ccf96270
Parks Cochrane <pdcochrane@gmail.com>
2022-05-01 23:39:06 -0400

    ugh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(cors());
 app.use(express.static("./public"));
 const args = require('minimist')(process.argv.slice(2))
 args['port']
-const port = args.port || process.env.PORT || 5000
+const port = args.port || process.env.PORT || 5555
 
 
 const server = app.listen(port, () => {
